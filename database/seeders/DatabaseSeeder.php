e1bf4577247cdc39f9078adaf1278c5418652594
Slobodan <slobodan.skaric@gmail.com>
2023-11-26 18:11:52 +0100

    Implement seeders for User,Admin,Coache

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Administrator;
+use App\Models\Coach;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //$this->call(UserSeeder::class);
+
+        User::factory(10)->create();
+        Administrator::factory(10)->create();
+        Coach::factory(10)->create();
     }
 }
